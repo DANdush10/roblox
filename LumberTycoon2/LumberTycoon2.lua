701709196d001c682b2922bbf220d522abf75b53
Dan Strauss <dan.strauss1000@gmail.com>
2025-04-26 15:13:20 +0300

    forgot to ctrl + s

--- LumberTycoon2/LumberTycoon2.lua
+++ LumberTycoon2/LumberTycoon2.lua
@@ -1,5 +1,5 @@
 local Workspace = game:GetService("Workspace")
-TreeRegions = {}
+local TreeRegions = {}
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 local PlayerGui = Player:WaitForChild("PlayerGui")
@@ -9,20 +9,40 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ClientIsDragging = ReplicatedStorage.Interaction.ClientIsDragging
 local AxeClasses = ReplicatedStorage.AxeClasses
 local chopRemote = ReplicatedStorage.Interaction.RemoteProxy
-
+local LooseTreeModel = nil
 local function getAxeStats(Name)
 local AxeStats = require(AxeClasses:FindFirstChild("AxeClass_"..Name)).new()
 return AxeStats
 end
 
-local function grabTree(Tree)
-print("Grab Tree...")
-local OrgPlayerPos = HRP.CFrame
-local args = {
+local function isMyTree(TreeType, child) -- ?
+if tostring(child:WaitForChild("Owner").Value) == Player.Name then return true
+
+else return false
+end
+end
+local function filterThroughWoodSection(Tree)
+    local biggestWoodSec = nil
+    local biggestWoodSize = 0
+    for _,v in pairs(Tree:GetChildren()) do
+        if v.Name == "WoodSection" then
+            if v.Size > biggestWoodSize then
+                biggestWoodSec = v
+                biggestWoodSize = v.Size
+            end
+        end
+    end
+    return biggestWoodSec
+end
+
+local function grabTree(Tree,OrgPlayerPos)
+print("Grabbing Tree...")
+local args = 
+{
     [1] = Tree
 }
 
-Tree.PrimaryPart = Tree.WoodSection
+Tree.PrimaryPart = Tree:WaitForChild("WoodSection")
 HRP.CFrame = Tree.PrimaryPart.CFrame
 for i = 1,100 do
 ClientIsDragging:FireServer(unpack(args))
@@ -39,31 +59,58 @@ HRP.CFrame = OrgPlayerPos + Vector3.new(0,20,0)
 
 end
 
-local function chopTree(Tree)
-local axeStats = getAxeStats(Players.LocalPlayer.ToolFolder.Tool.Value)
+local function chopTree(Tree, OrgPlayerPos)
+    local Tool = Player.Backpack:FindFirstChild("Tool")
+    if Tool then
+    Tool.Parent = Player.Character
+    else
+    print("Tool Not Found")
+    end
+local axeStats = getAxeStats(Player.Character.Tool:WaitForChild("ToolName").Value)
 local args = 
 {
     [1] = 
     Tree.CutEvent,
     [2] = 
     {
-    tool = Players.LocalPlayer.ToolFolder.Tool,
-    height = 0.3,
-    faceVector = Vector3.new(1, 0, 0),
-    sectionId = 1,
-    hitPoints = axeStats.Damage,
-    cooldown = axeStats.SwingCooldown,
-    cuttingClass = "Axe"
+    ["tool"] = Player.Character.Tool,
+    ["height"] = 0.3,
+    ["faceVector"] = Vector3.new(1, 0, 0),
+    ["sectionId"] = 1,
+    ["hitPoints"] = axeStats.Damage,
+    ["cooldown"] = axeStats.SwingCooldown,
+    ["cuttingClass"] = "Axe"
     }
 }
+
 HRP.CFrame = Tree.WoodSection.CFrame
+
+
+local connection
+connection = Workspace.LogModels.ChildAdded:Connect(function(child)
+    if isMyTree(Tree, child) then
+        print(" Tree Found!")
+        LooseTreeModel = child
+        grabTree(LooseTreeModel, OrgPlayerPos)
+        connection:Disconnect()
+    else
+        print("Not My tree :(")
+    end
+end)
 repeat
+
 chopRemote:FireServer(unpack(args))
 task.wait(axeStats.SwingCooldown)
-print("Trying"..axeStats.SwingCooldown)
-until Tree == nil
 
+until not(Tree:FindFirstChild("CutEvent"))
 end
+
+
+local function getTree(Tree)
+    local OrgPlayerPos = HRP.CFrame
+    chopTree(Tree,OrgPlayerPos)
+end
+
 
 local function initTreeGUI()
     local screenGui = Instance.new("ScreenGui")
@@ -143,8 +190,7 @@ local function initTreeGUI()
         -- Click handler (custom logic placeholder)
         button.MouseButton1Click:Connect(function()
             local Tree = region:FindFirstChild("Model")
-            local WoodSection = Tree:FindFirstChild("WoodSection")
-            HRP.CFrame = WoodSection.CFrame
+            getTree(Tree)
         end)
     end
 
@@ -174,14 +220,14 @@ local function RenameTreeRegions()
     for i,v in pairs(Workspace:GetChildren()) do
         if v.Name == "TreeRegion" then
             if v:FindFirstChild("Model") then
-            v.Name = v:FindFirstChild("Model"):FindFirstChild("TreeClass").Value
-            table.insert(TreeRegions, v)
+                if v:FindFirstChild("Model"):FindFirstChild("TreeClass") then
+                    v.Name = v:FindFirstChild("Model"):FindFirstChild("TreeClass").Value
+                    table.insert(TreeRegions, v)
             
+                end
             end
         end
     end
     initTreeGUI()
-    chopTree(Workspace.Birch.Model)
 end
 RenameTreeRegions()
-
